39d1ebdbd86e911860d98a93f206474d306a5866
Kevin Wright <kev.lee.wright@gmail.com>
2011-03-21 16:09:26 +0000

    Completed all main java->scala migration.  Still remaining: enums, JavaCC parser & tests

---
+++ src/main/scala/com/google/visualization/datasource/query/parser/QueryBuilder.scala
@@ -0,0 +1,11 @@
+package com.google.visualization.datasource.query.parser
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kevin
+ * Date: 21/03/11
+ * Time: 11:13
+ * To change this template use File | Settings | File Templates.
+ */
+
+class QueryBuilder
